33b3c24940b24894f29b9b13ea4082345d9e9be5
jesusrlv <jesusrlvrojo@gmail.com>
2022-01-25 13:12:25 -0600

    bd store

---
+++ store_cel.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 25-01-2022 a las 19:59:48
+-- Versión del servidor: 10.1.37-MariaDB
+-- Versión de PHP: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `store_cel`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envios`
+--
+
+CREATE TABLE `envios` (
+  `id` int(11) NOT NULL,
+  `fecha_registro` date NOT NULL,
+  `compania` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `fecha_llegada` date NOT NULL,
+  `venta_general` int(11) NOT NULL,
+  `id_envio` int(11) NOT NULL COMMENT 'id de la persona que envio',
+  `costo_envio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `precio` int(11) NOT NULL,
+  `imagen` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `total_vendido` int(11) NOT NULL,
+  `codigo` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usr`
+--
+
+CREATE TABLE `usr` (
+  `id` int(11) NOT NULL,
+  `usr` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `pwd` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `perfil` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta_gral`
+--
+
+CREATE TABLE `venta_gral` (
+  `id` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `fecha_venta` date NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `tarjeta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta_individual`
+--
+
+CREATE TABLE `venta_individual` (
+  `id` int(11) NOT NULL,
+  `producto` int(11) NOT NULL,
+  `fecha_venta` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `envios`
+--
+ALTER TABLE `envios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usr`
+--
+ALTER TABLE `usr`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `venta_gral`
+--
+ALTER TABLE `venta_gral`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `venta_individual`
+--
+ALTER TABLE `venta_individual`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `envios`
+--
+ALTER TABLE `envios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usr`
+--
+ALTER TABLE `usr`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta_gral`
+--
+ALTER TABLE `venta_gral`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta_individual`
+--
+ALTER TABLE `venta_individual`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
