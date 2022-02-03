885d49b1d6fd3263004d277668cb4f3bee69621d
jesus rlv <jesusrlv@iMac-de-jesus.local>
2022-02-03 16:00:11 -0600

    funcionando el js de onchange, para carrito

--- query/compra.js
+++ query/compra.js
@@ -1,6 +1,6 @@
 function agregarCarrito(){
 
-    document.getElementById("compracarrito"+elem).innerHTML+='<div class="input-group"><input type="text" class="form-control" placeholder="Redacta la pregunta" aria-label="Username" aria-describedby="basic-addon1"><span class="input-group-text" id="basic-addon1"><i class="bi bi-question-circle-fill"></i></span></div>';
+    document.getElementById('compracarrito').innerHTML+='<div class="input-group"><input type="text" class="form-control" placeholder="Redacta la pregunta" aria-label="Username" aria-describedby="basic-addon1"><span class="input-group-text" id="basic-addon1"><i class="bi bi-question-circle-fill"></i></span></div>';
 
 }
 
