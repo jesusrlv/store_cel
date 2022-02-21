cccc9ae66b501ce23140810bb5673c072f17fca1
jesusrlv <jesusrlvrojo@gmail.com>
2022-02-21 15:44:40 -0600

    contador de productos

--- canvas.php
+++ canvas.php
@@ -12,15 +12,15 @@
   <div class="offcanvas-body border-bottom">
   
         <div class="row-12">
-            <form action="query/compras.php" method="POST" class="border-bottom" onchange="cambioInput()">
+          <form action="query/compras.php" method="POST" class="" onchange="cambioInput()">
                 <p id="compracarrito"></p>
-                <hr class="w-100 justify-aligment-center">
+                <!-- <hr class="w-100 justify-aligment-center"> -->
                                     
-            </form>
-            <button class="btn btn-primary w-100" id="btncerrar"><i class="bi bi-cart-plus" onclick="cambiarHidden()"></i> Pagar carrito</button>
+            <!-- </form> -->
+            <button class="btn btn-primary w-100" type="button" id="btncerrar" onclick="cambiarHidden()"><i class="bi bi-cart-plus"></i> Pagar carrito</button>
 
             <div class="col-12 mt-2">
-                <strong>Número de productos:</strong> ###
+                <strong>Número de productos: </strong><span id="contadorInputs"></span>
                 </div>
                 
                 <div class="col-12">
@@ -44,29 +44,36 @@
             </div>
             <div class="mb-3">
               <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-phone"></i> Teléfono</small></label>
-              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Teléfono...">
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Teléfono..." onkeypress="ValidaSoloNumeros()">
             </div>
             <div class="mb-3">
               <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-envelope-fill"></i> Correo electrónico</small></label>
               <input type="email" class="form-control" id="formGroupExampleInput" placeholder="Correo electrónico...">
             </div>
             <div class="mb-3">
-              
               <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-credit-card-2-back"></i> Tarjeta para pago</small></label>
               <div class="input-group">
-              <input type="text" class="form-control w-75" id="formGroupExampleInput" placeholder="Tarjeta...">
-              <input type="password" class="form-control w-25" id="formGroupExampleInput" placeholder="NIP...">
+                <input type="text" class="form-control w-70" id="tarjetaNum" maxlength ="16" placeholder="XXXX-XXXX-XXXX-XXXX" onkeypress="ValidaSoloNumeros()">
+                <input type="text" class="form-control w-30" id="tipoTarjeta" readonly>
+              </div>
+              <input type="text" class="form-control w-100" id="formGroupExampleInput" placeholder="Nombre en tarjeta...">
+              
+              <div class="input-group">
+              <input type="text" class="form-control w-40" id="formGroupExampleInput" placeholder="Expira Mes..." maxlength="2" onkeypress="ValidaSoloNumeros()">
+              <input type="text" class="form-control w-40" id="formGroupExampleInput" placeholder="Expira Año..." maxlength="2" onkeypress="ValidaSoloNumeros()">
+              <input type="password" class="form-control w-20" id="formGroupExampleInput" placeholder="NIP..." maxlength="3" onkeypress="ValidaSoloNumeros()">
               </div>
             </div>
           </div>
           <button type="submit" class="btn btn-primary w-100"><i class="bi bi-cart-plus"></i> Realizar pago</button>  
+          </form>
         </div>
     
   </div>
 </div>
 
 <script>
-  function cambioInput(){
+  function cambioInput() {
     var inputs = document.getElementById("offcanvasRight").getElementsById("contadorId");
     alert("Valor recibido: " + inputs);
   }
@@ -80,7 +87,51 @@ function cambiarHidden(){
   
   // https://www.w3schools.com/jsref/prop_style_visibility.asp
 }
+
+//Función que permite solo Números
+function ValidaSoloNumeros() {
+ if ((event.keyCode < 48) || (event.keyCode > 57)) 
+  event.returnValue = false;
+}
 </script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<!-- https://es.stackoverflow.com/questions/70426/buscar-numero-de-tarjeta-de-credito-en-un-input-o-textbox-posterior-a-esto-remo -->
+<script>
+  function getCardType(cardNo) {
+  var cards = {
+    "American Express": /^3[47][0-9]{13}$/,
+    "Mastercard": /^5[1-5][0-9]{14}$/,
+    "Visa": /^4[0-9]{12}(?:[0-9]{3})?$/
+  };
+  
+  for(var card in cards) {
+    if (cards[card].test(cardNo)) {
+      return card;
+    }
+  }
+  
+  return undefined;
+}
+
+// $('input').on('change', function() {
+$('input[id="tarjetaNum"]').on('change', function() {
+  var value = $(this).val().trim();
+  
+  var cardType = getCardType(value);
+  
+  if (!cardType) {
+    alert('tarjeta invalida');
+  } else {
+    // alert('tarjeta tipo:' + cardType);
+    document.getElementById('tipoTarjeta').value = cardType;
+    $(this).val(Array(value.length-4).join("X")+value.substring(value.length-4));
+  }      
+});
+</script>
+
+<!-- 4915663435988911 -->
+<!-- 5579100275123121 -->
 
 <!-- ejemplos de cards -->
 <!-- https://bbbootstrap.com/snippets/bootstrap-5-simple-information-card-85881560 -->
--- query/compra.js
+++ query/compra.js
@@ -10,14 +10,28 @@ function agregarCarrito(x1,nombre,costo){
     // alert("Valor recibido: " + contador);
     // var y1 = y;
     // console.log('x1');
-    document.getElementById('compracarrito').innerHTML+='<div id="limpiarcompra'+contador+'" class="input-group mb-1"><input name="nombreproducto'+contador+'" type="text" class="form-control w-50" placeholder="" aria-label="Username" aria-describedby="basic-addon1" value="'+y+'" READONLY><input name="valor" type="text" class="form-control text-center" placeholder="" aria-label="Username" aria-describedby="basic-addon1" value="$'+precio+'" READONLY><input name="valor" type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" value="'+x+'" id="contadorId" hidden><button type="button" class="btn btn-danger input-group-text" onclick="borrarCompras('+contador+')"><i class="bi bi-x-circle-fill"></i></button></div>';
+    document.getElementById('compracarrito').innerHTML+='<div id="limpiarcompra'+contador+'" class="input-group mb-1"><input name="nombreproducto'+contador+'" type="text" class="form-control w-50" placeholder="" aria-label="Username" aria-describedby="basic-addon1" value="'+y+'" READONLY><input name="valor" id="producto" type="text" class="form-control text-center" placeholder="" aria-label="Username" aria-describedby="basic-addon1" value="$'+precio+'" READONLY><input name="valor" type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1" value="'+x+'" id="contadorId" hidden><button type="button" class="btn btn-danger input-group-text" onclick="borrarCompras('+contador+')"><i class="bi bi-x-circle-fill"></i></button></div>';
 
+    var divs = document.querySelectorAll('input[id="producto"]');
+    var valor = divs.length;
+    // alert(valor);
+    document.getElementById('contadorInputs').textContent = valor;    
+   
 }
 
 function borrarCompras(contador){
     let cont = contador;
     document.getElementById("limpiarcompra"+cont).innerHTML = "";
+
+    var divs = document.querySelectorAll('input[id="producto"]');
+    var valor = divs.length;
+    // alert(valor);
+    document.getElementById('contadorInputs').textContent = valor;  
 }
+
+// var inputs = document.querySelectorAll('input[id="producto"]');
+
+// document.getElementById('contadorInputs').value = inputs;
 
 // accesosadmin.html
 // línea 473 | código para hacer un for de los inputs
