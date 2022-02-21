976f81178dcb35f1b1e260620d99e08288072775
jesus rlv <jesusrlv@iMac-de-jesus.local>
2022-02-21 00:54:27 -0600

    agregado el hidden, realizar num prod, total, pago y back

--- canvas.php
+++ canvas.php
@@ -15,18 +15,51 @@
             <form action="query/compras.php" method="POST" class="border-bottom" onchange="cambioInput()">
                 <p id="compracarrito"></p>
                 <hr class="w-100 justify-aligment-center">
-                <button type="submit" class="btn btn-primary w-100"><i class="bi bi-cart-plus"></i> Comprar</button>                    
+                                    
             </form>
+            <button class="btn btn-primary w-100" id="btncerrar"><i class="bi bi-cart-plus" onclick="cambiarHidden()"></i> Pagar carrito</button>
 
             <div class="col-12 mt-2">
-                    <strong>Número de productos:</strong> ###
+                <strong>Número de productos:</strong> ###
                 </div>
+                
                 <div class="col-12">
                     <strong>Total: </strong> $$$
                 </div>
+                
                 <div class="4">
-                    
-                </div>
+            </div>
+        </div>
+
+        <div class="row-12 border-top mt-3" id="cambiohidden">
+
+          <div class="col mt-2">
+            <div class="mb-3">
+              <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-person-circle"></i> Nombre completo</small></label>
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nombre...">
+            </div>
+            <div class="mb-3">
+              <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-card-heading"></i> Dirección</small></label>
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Dirección...">
+            </div>
+            <div class="mb-3">
+              <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-phone"></i> Teléfono</small></label>
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Teléfono...">
+            </div>
+            <div class="mb-3">
+              <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-envelope-fill"></i> Correo electrónico</small></label>
+              <input type="email" class="form-control" id="formGroupExampleInput" placeholder="Correo electrónico...">
+            </div>
+            <div class="mb-3">
+              
+              <label for="formGroupExampleInput" class="form-label"><small><i class="bi bi-credit-card-2-back"></i> Tarjeta para pago</small></label>
+              <div class="input-group">
+              <input type="text" class="form-control w-75" id="formGroupExampleInput" placeholder="Tarjeta...">
+              <input type="password" class="form-control w-25" id="formGroupExampleInput" placeholder="NIP...">
+              </div>
+            </div>
+          </div>
+          <button type="submit" class="btn btn-primary w-100"><i class="bi bi-cart-plus"></i> Realizar pago</button>  
         </div>
     
   </div>
@@ -37,6 +70,16 @@
     var inputs = document.getElementById("offcanvasRight").getElementsById("contadorId");
     alert("Valor recibido: " + inputs);
   }
+</script>
+
+<script>
+  document.getElementById('cambiohidden').style.visibility = 'hidden';
+function cambiarHidden(){
+  document.getElementById('cambiohidden').style.visibility = 'visible';
+  document.getElementById('btncerrar').style.visibility = 'hidden';
+  
+  // https://www.w3schools.com/jsref/prop_style_visibility.asp
+}
 </script>
 
 <!-- ejemplos de cards -->
